4800fd80e1a2e86206fd09d0047717309c0c61ac
Dan Garland <dan@dangarland.co.uk>
2014-05-13 11:17:06 +0100

    Adding database cleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,21 @@
 RACK_ENV = 'test' unless defined?(RACK_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
-RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 # You can use this method to custom specify a Rack app
@@ -18,3 +31,5 @@ def app(app = nil, &blk)
   @app ||= block_given? ? app.instance_eval(&blk) : app
   @app ||= Padrino.application
 end
+
+
