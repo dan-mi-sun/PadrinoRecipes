bdc9eca2fbda3b0f163ef8f7d64504fddad67bd8
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-14 18:12:54 +0100

    add nested ingredient fields feature

--- app/controllers/recipes.rb
+++ app/controllers/recipes.rb
@@ -9,6 +9,7 @@ PadrinoRecipies::App.controllers :recipes do
 
   get :new do
     @recipe = Recipe.new
+    3.times { @recipe.ingredient_recipes.build(:ingredient => Ingredient.new) }
     render :'recipes/new'
   end
 
